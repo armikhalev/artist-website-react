8f602ebecc9ebc2456fa8891d39831fa12def825
Arseny Mikhalev <arseny@Arsenys-MacBook-Pro.local>
2017-07-20 13:16:03 -0400

    Refactoring: puts logic onWheel event to menu.js

--- artist-website/src/App.js
+++ artist-website/src/App.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Link } from 'react-router-dom';
 
 import { Router } from './Router';
-import { Zoomer } from './functionals/zoomer';
+import { Menu } from './components/menu';
+// import { Zoomer } from './functionals/zoomer';
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
-        <Zoomer>
+        {/*<Zoomer>*/}
+          <Menu>
+            <Link to='/' ref={elem => this.clickHomeLink = elem}>Home</Link>
+            <Link to='/ipad' className="zoom-area">iPad</Link>
+            <Link to='/sketchbook' className="zoom-area">Sketchbook</Link>
+            <Link to='/phone' className="zoom-area">Phone</Link>
+          </Menu>
           <Router />
-          <div data-link-name="ipad-link" className="zoom-area"></div>
+
+          {/*<div data-link-name="ipad-link" className="zoom-area"></div>
           <div data-link-name="sketchbook-link" className="zoom-area"></div>
-          <div data-link-name="phone-link" className="zoom-area"></div>
-        </Zoomer>
+          <div data-link-name="phone-link" className="zoom-area"></div>*/}
+        {/*</Zoomer>*/}
       </div>
     );
   }
--- artist-website/src/components/menu.js
+++ artist-website/src/components/menu.js
@@ -1,15 +1,28 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { Component } from 'react';
 
 // The Header creates links that can be used to navigate
 // between routes.
-export const Menu = () => (
-  <nav style={{display:"none"}}>
-      <ul>
-          <li><Link to='/' id="home-link">Home</Link></li>
-          <li><Link to='/ipad' id="ipad-link">iPad</Link></li>
-          <li><Link to='/sketchbook' id="sketchbook-link">Sketchbook</Link></li>
-          <li><Link to='/phone' id="phone-link">Phone</Link></li>
-      </ul>
-  </nav>
-);
+export class Menu extends Component {
+
+    handleClick(e) {
+        e.preventDefault();
+
+        // If zooming in get to the zoomed object link
+        if (e.deltaY > 0) {
+            var clicked = e.target;
+        }
+        // On zoom out get back to home-table
+        else if (e.deltaY <= 0) {
+           
+        }
+
+        // Check link to be valid, not null or undefined
+        if (clicked) {
+            clicked.click();
+        }
+    }
+    render () {
+        return ( <div onWheel={this.handleClick}>{this.props.children}</div> );
+    }
+
+}
