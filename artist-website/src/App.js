7a0471ae2917be929d389ec5ca63edab338c0afb
Arseny Mikhalev <arseny@Arsenys-MacBook-Pro.local>
2017-07-25 13:00:39 -0400

    Refactoring: use React ref to get 'menu' DOM element for cilk().

--- artist-website/src/App.js
+++ artist-website/src/App.js
@@ -4,29 +4,22 @@ import { Link } from 'react-router-dom';
 
 import { Router } from './Router';
 import { Menu } from './components/menu';
-// import { Zoomer } from './functionals/zoomer';
 
 class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        {/*<Zoomer>*/}
-          <Menu>
-            <Link to='/' ref={elem => this.clickHomeLink = elem}>Home</Link>
-            <Link to='/ipad' className="zoom-area">iPad</Link>
-            <Link to='/sketchbook' className="zoom-area">Sketchbook</Link>
-            <Link to='/phone' className="zoom-area">Phone</Link>
-          </Menu>
-          <Router />
-
-          {/*<div data-link-name="ipad-link" className="zoom-area"></div>
-          <div data-link-name="sketchbook-link" className="zoom-area"></div>
-          <div data-link-name="phone-link" className="zoom-area"></div>*/}
-        {/*</Zoomer>*/}
-      </div>
+		<div className="App">
+			<Menu>
+				<Link to='/ipad' className="zoom-area">iPad</Link>
+				<Link to='/sketchbook' className="zoom-area">Sketchbook</Link>
+				<Link to='/phone' className="zoom-area">Phone</Link>
+			</Menu>
+			<Router />
+		</div>
     );
   }
+
 }
 
 export default App;
--- artist-website/src/components/menu.js
+++ artist-website/src/components/menu.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 // The Header creates links that can be used to navigate
 // between routes.
 export class Menu extends Component {
+
+	constructor(props) {
+		super(props);
+		this.handleClick = this.handleClick.bind(this);
+	}
 
     handleClick(e) {
         e.preventDefault();
@@ -13,16 +19,25 @@ export class Menu extends Component {
         }
         // On zoom out get back to home-table
         else if (e.deltaY <= 0) {
-           
+			clicked = this.clickHomeLink.children[0];
         }
-
         // Check link to be valid, not null or undefined
         if (clicked) {
             clicked.click();
         }
     }
+	
     render () {
-        return ( <div onWheel={this.handleClick}>{this.props.children}</div> );
+        return ( 
+				<div onWheel={this.handleClick}>
+	            {/*ref must be in div otherwise it is not a DOM element but React object			*/}
+                <div  ref={(elem) => {this.clickHomeLink = elem} }>
+					<Link to='/'></Link>
+				</div>
+                {this.props.children}
+            </div> 
+            );
     }
 
 }
+  
